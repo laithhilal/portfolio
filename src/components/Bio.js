618d9f03c491b1acc366e4555ab8296a1fc23d56
Laith <laith.hilal@appliedtechnology.se>
2023-03-24 04:02:46 +0300

    grammar

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -3,7 +3,7 @@ import './Bio.css';
 
 const Bio = () => {
   return (
-    <div id='bio'>I am a skilled software developer with a focus on full stack web development. I recently graduated from the School of Applied Technology's Full Stack JavaScript program, where I developed a strong foundation in React, JavaScript, and TypeScript. This intensive three-month training program emphasized Test-Driven Development (TDD), mob programming, and applied learning, equipping me with the skills necessary to create elegant, user-friendly applications.</div>
+    <div id='bio'>Skilled software developer with a focus on full stack web development. I recently graduated from the School of Applied Technology's Full Stack JavaScript program, where I developed a strong foundation in React, JavaScript, and TypeScript. This intensive three-month training program emphasized Test-Driven Development, mob programming, and applied learning, equipping me with the skills necessary to create elegant, user-friendly applications.</div>
   )
 }
 
