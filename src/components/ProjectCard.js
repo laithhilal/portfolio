bdec1777fdfdc4d591163c7c8ab3a6558979d432
Laith <laith.hilal@appliedtechnology.se>
2023-03-24 03:58:48 +0300

    added arrows

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 const ProjectCard = ({ project }) => {
+    
   const [techStackVisible, setTechStackVisible] = useState(false);
 
   return (
@@ -8,27 +9,30 @@ const ProjectCard = ({ project }) => {
       <img src={project.imageUrl} alt={project.name} />
       <h3>{project.name}</h3>
       <p>{project.description}</p>
-      <div className='card-footer'>
-        <div
-          className='tech-stack'
-          onClick={() => setTechStackVisible(!techStackVisible)}
+      <div
+        className='tech-stack'
+        onClick={() => setTechStackVisible(!techStackVisible)}
+      >
+        Tech Stack
+        {techStackVisible ? (
+          <i className='fas fa-angle-down' style={{ marginLeft: '5px' }}></i>
+        ) : (
+          <i className='fas fa-angle-up' style={{ marginLeft: '5px' }}></i>
+        )}
+        {techStackVisible && (
+          <div className='tech-stack-dropdown'>
+            {project.techStack}
+          </div>
+        )}
+      </div>
+      <div className='project-links'>
+        <a
+          href={project.githubUrl}
+          target='_blank'
+          rel='noopener noreferrer'
         >
-          Tech Stack
-          {techStackVisible && (
-            <div className='tech-stack-dropdown'>
-              {project.techStack}
-            </div>
-          )}
-        </div>
-        <div className='project-links'>
-          <a
-            href={project.githubUrl}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <i className='fab fa-github'></i>
-          </a>
-        </div>
+          <i className='fab fa-github'></i>
+        </a>
       </div>
     </div>
   );
